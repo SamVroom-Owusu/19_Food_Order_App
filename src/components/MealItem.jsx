70bfd344734816aad7caddc7afe0a97771b6a49b
Sam V-O <sam.v.owusu@gmail.com>
2026-06-15 15:10:49 +0100

    added cart, cartItem and menuItem

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../utils/formatting";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
   const imgURL = `http://localhost:3000/${meal.image}`;
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -15,7 +23,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
